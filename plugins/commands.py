a37d91c2a664c55c89712bbb14ce39381d88faac
walter2da <jdtnursing8@gmail.com>
2026-03-14 20:54:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1071,7 +1071,7 @@ async def set_tutorial(client, message: Message):
     except IndexError:
         return await message.reply_text(
             f"<b>ᴄᴏᴍᴍᴀɴᴅ ɪɴᴄᴏᴍᴘʟᴇᴛᴇ !!\n\nᴜꜱᴇ ʟɪᴋᴇ ᴛʜɪꜱ -</b>\n\n"
-            f"<code>/{message.command[0]} https://t.me/dreamxbotz</code>"
+            f"<code>/{message.command[0]} https://t.me/iqbaleditzzz</code>"
         )
     if message.command[0] == "set_tutorial":
         tutorial_key = "tutorial"
