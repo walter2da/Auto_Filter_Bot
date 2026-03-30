87095fc93ae82a323af01f14d990a5e0ed816d8c
Walter 🤍 <133798451+walterwhite-org@users.noreply.github.com>
2026-03-30 13:37:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ INDEX_CAPTION = bool(environ.get('SAVE_CAPTION', False)) # Save caption db when 
 COVERX = bool(environ.get('COVERX', False)) # Use cover image for indexed files (default: True)
 # If you disable it then bot will use a default thumb for all files
 
-PICS_URL = (environ.get('PICS', 'https://walter-white-73c8.rihanrazak765.workers.dev/')).split() #random anime girl img each time from aniwallpaper (Experimental)
+PICS_URL = (environ.get('PICS', 'https://fancy-mud-efba.rihanrazak765.workers.dev')).split() #random anime girl img each time from aniwallpaper (Experimental)
 PICS = (environ.get('PICS', 'https://graph.org/file/1fc1c4781bf5424b750f7-18041436254db31340.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
